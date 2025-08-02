4123201eb1118b8e05136d3745c7608273212831
niloofar-samei <niloofar.samei@gmail.com>
2025-08-02 13:17:21 +0100

    Cleanup.

--- series/urls.py
+++ series/urls.py
@@ -9,11 +9,7 @@ from series.views import (
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="IndexListView"),
-    #    path("movie/<int:movie_id>/", views.movie, name="movie"),
-    #    path("new/", views.new, name="new"),
     path("movie/new/", MovieCreateView.as_view(), name="MovieCreateView"),
     path("movie/<int:pk>/", MovieDetailView.as_view(), name="MovieDetailView"),
     path("movie/<int:pk>/delete/", MovieDeleteView.as_view(), name="MovieDeleteView"),
-    #    path("delete/<int:movie_id>", views.delete, name="delete"),
-    #    path("voteup/<int:movie_id>", views.voteup, name="voteup"),
 ]
