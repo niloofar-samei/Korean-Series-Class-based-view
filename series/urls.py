260d04bae18c27e0bd99ee473c149ee97ef4de38
niloofar-samei <niloofar.samei@gmail.com>
2025-08-02 12:03:01 +0100

    Create class MovieCreateView.

--- series/urls.py
+++ series/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
+from series.views import (
+    IndexListView,
+    MovieCreateView,
+)
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="IndexListView"),
     #    path("movie/<int:movie_id>/", views.movie, name="movie"),
     #    path("new/", views.new, name="new"),
-    #    path("new_django_form", views.new_django_form, name="new_django_form"),
+    path("movie/new/", MovieCreateView.as_view(), name="MovieCreateView"),
     #    path("delete/<int:movie_id>", views.delete, name="delete"),
     #    path("voteup/<int:movie_id>", views.voteup, name="voteup"),
 ]
